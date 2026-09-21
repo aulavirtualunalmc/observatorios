9f8006bad88ddc850eaa51ce5978f1b0dbb61495
Daniel Ochoa <daniel.ochoa@sao6.com.co>
2026-09-21 11:28:58 -0500

    adasd

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
+import node from '@astrojs/node';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
 	output: 'server',
-	adapter: vercel(),
+	adapter: node({
+		mode: 'standalone',
+	}),
 
 
 	prefetch: {
